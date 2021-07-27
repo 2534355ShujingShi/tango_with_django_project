0e4e70a231f569683a8531201c5f3c71417762b8
Shujing Shi <2534355@student.gla.ac.uk>
2021-07-27 22:10:36 +0100

    Chapter3 Exsercise Fisrt Time

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path ('', views.index,name='index'),
+   # path ('', views.index,name='index'),
+    path ('', views.about,name='about'),
 ]
+
--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    #<a href ='/rango/about/'>About</a>
     return HttpResponse("Rango says hey there partner!")
+
+
+def about(request):
+     #<a href ='/rango/index/'>Index</a>
+    return HttpResponse("Rango says here is the about page.")
+
